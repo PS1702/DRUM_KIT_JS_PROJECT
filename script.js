457218b57593cfede213fe20789f8f6b75ed1141
Pratiksha <pratikshashelke17@gmail.com>
2022-12-28 18:17:21 +0530

    Adding all file to commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function play1(){
+    let audio = document.getElementById("audio1");
+    audio.play();
+}
+
+$(document.body).on('keydown', function(e){
+    if(e.keyCode == 52){
+      $('#1').get(0).click();
+    }
+  });
+
+function play2(){
+    let audio = document.getElementById("audio2");
+    audio.play()
+}
+
+function play3(){
+    let audio = document.getElementById("audio3");
+    audio.play()
+}
+
+function play4(){
+    let audio = document.getElementById("audio4");
+    audio.play()
+}
+
+function play5(){
+    let audio = document.getElementById("audio5");
+    audio.play()
+}
+
+function play6(){
+    let audio = document.getElementById("audio6");
+    audio.play()
+}
+
+function play7(){
+    let audio = document.getElementById("audio7");
+    audio.play()
+}
+
+function play8(){
+    let audio = document.getElementById("audio8");
+    audio.play()
+}
+
+function play9(){
+    let audio = document.getElementById("audio9");
+    audio.play()
+}
